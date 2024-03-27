373aeb48f2825e2e111b4c0126c721bfc83a83bc
mattiapomelli <mattiapomelli@gmail.com>
2024-03-26 18:58:12 -0700

    Update env variables

--- src/env.mjs
+++ src/env.mjs
@@ -1,13 +1,7 @@
 import { createEnv } from "@t3-oss/env-nextjs";
-import { z } from "zod";
 
 export const env = createEnv({
-  server: {
-    OPENAI_API_KEY: z.string().min(1),
-    GROQ_API_KEY: z.string().min(1),
-    BRAVE_SEARCH_API_KEY: z.string().min(1),
-    SERPER_API_KEY: z.string().min(1),
-  },
+  server: {},
   client: {},
   skipValidation: process.env.SKIP_ENV_VALIDATION === "true",
   experimental__runtimeEnv: {},
